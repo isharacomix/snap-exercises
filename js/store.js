897b485e8d7569e645b4f9dd012429083c31b2dc
Barry Peddycord III <ishara@isharacomix.org>
2014-06-04 11:31:25 -0400

    I LIKE SERIAL

--- js/store.js
+++ js/store.js
@@ -1364,7 +1364,7 @@ StageMorph.prototype.toXML = function (serializer) {
             '<variables>%</variables>' +
             '</project>',
         (ide && ide.projectName) ? ide.projectName : 'Untitled',
-        serializer.app,,
+        serializer.app,
         serializer.version,
         (ide && ide.projectNotes) ? ide.projectNotes : '',
         thumbdata,
