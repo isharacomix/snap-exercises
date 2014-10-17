aff4f4adab3db6445079e5b55bdfe6a0bd6a6408
Barry Peddycord III <ishara@isharacomix.org>
2014-10-17 14:36:55 -0400

    changes

--- data.php
+++ data.php
@@ -7,6 +7,11 @@ if ( empty($_SESSION['user']))
    $_SESSION['user'] = hash("md5",$_SERVER['HTTP_CLIENT_IP'])."_".(rand());
 }
 $me = $_SESSION['user'];
+if (isset($_SESSION['username']))
+{
+    $me = $me."_".$_SESSION['username'];
+}
+
 
 $last = urldecode(file_get_contents("php://input")) ;
 $last = preg_replace('/data\:.*?;base64.*?\<\//', 'null</', $last);
